386b5890bce481d5f54580a6ce1d05b042214390
jameslyrech-byte <jameslyrech-byte@users.noreply.github.com>
2026-07-08 13:11:35 +0800

    tech(settings): configure_settings

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'core.User'
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
